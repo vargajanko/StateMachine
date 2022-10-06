7a8030c783503ca8d1bfbfd3dea57e1c8f4e5658
Janko Varga <varga.janko@yahoo.de>
2022-10-06 03:37:23 +0200

    add false test case

--- TESTS/UnitTests.cpp
+++ TESTS/UnitTests.cpp
@@ -1,4 +1,4 @@
-//#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.h"
 
 unsigned int Factorial(unsigned int number) {
@@ -6,7 +6,7 @@ unsigned int Factorial(unsigned int number) {
 }
 
 TEST_CASE("Factorials are computed", "[factorial]") {
-    REQUIRE(Factorial(1) == 1);
+    REQUIRE(Factorial(1) == 2);
     REQUIRE(Factorial(2) == 2);
     REQUIRE(Factorial(3) == 6);
     REQUIRE(Factorial(10) == 3628800);
